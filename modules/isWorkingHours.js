6391ff921de5dfbd66b39481f7af4949556a7007
Norbert Tóth <norbert@quince.hu>
2018-07-04 01:33:47 +0200

    Create isWorkingHours function - checks if a time value is in the working hours range - uses the config object

---
+++ modules/isWorkingHours.js
@@ -0,0 +1,23 @@
+/**
+ * Returns true or false depending on if the input
+ * date object is representing a moment in range of
+ * working hours and working week days.
+ *
+ * @param {Object} time a javascript Date object
+ * @returns {boolean} true if inside working hours range
+ */
+const isWorkingHours = (time, working) => {
+    const hours = time.getHours();
+    const day = time.getDay();
+    if (
+        hours >= working.hours.start &&
+        hours <= working.hours.end &&
+        day >= working.week.start &&
+        day <= working.week.end
+    ) {
+        return true;
+    }
+    return false;
+};
+
+export default isWorkingHours;
