bc36e5facd6926d552a10c182986e18acfc47247
Norbert Tóth <norbert@quince.hu>
2018-07-04 01:31:44 +0200

    Create separate config object as a ES module for working hours

---
+++ modules/config.js
@@ -0,0 +1,16 @@
+/**
+ * Configuration object representing working hours and days.
+ * Days are numbers, where 0 is Sunday and 6 is Saturday.
+ */
+const emarsys = {
+    hours: {
+        start: 9,
+        end: 17,
+    },
+    week: {
+        start: 1,
+        end: 5,
+    },
+};
+
+export default emarsys;
