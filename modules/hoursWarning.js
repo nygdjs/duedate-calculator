3e670fb05929223960f569c7c4b6bbd13ca3967d
Norbert Tóth <norbert@quince.hu>
2018-07-04 01:54:49 +0200

    Make warning message config independent - convert it to a function - working-hours config object is the only param

--- modules/hoursWarning.js
+++ modules/hoursWarning.js
@@ -1,11 +1,20 @@
-import emarsys from './config';
+/**
+ * Returns a string template literal for warn user if entered
+ * date for start outside of working hours.
+ * Dynamically populated from working hours config object.
+ *
+ * @param {Object} working a object of working hours and days
+ *                         (see eg. ./config.js)
+ * @returns {string} Warning message for eg. logging in console
+ */
 
-const hoursWarning = `
+const hoursWarning = working => `
     It looks like someone is doing overtime... 💪
     Seriously...
     You entered a start date, that is outside of working hours.
-    Please set something between ${emarsys.hours.start} - ${emarsys.hours.end} hours
-    and 
+    Please set something between ${working.hours.start} - ${working.hours.end} hours and ${working.week.start} - ${working.week.end}.
 `;
 
 export default hoursWarning;
+
+// TODO: convert working weekdays to human readable strings
