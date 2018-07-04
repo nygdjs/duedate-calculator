e1458de6504f792e8dde649c57843cfbc5afdb07
Norbert Tóth <norbert@quince.hu>
2018-07-04 23:15:22 +0200

    Rename index.js to .mjs - Remove emoji from title too, because favicon

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import emarsys from './calculateDueDate/config.js';
 import CalculateDueDate from './calculateDueDate/index.js';
+const testDate5 = new Date('July 3 2018 10:30');
 
-console.log(CalculateDueDate(16, emarsys));
+console.log(CalculateDueDate(16, emarsys, testDate5));
