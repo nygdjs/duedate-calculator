0fd6146788133812286bedd8784b55dcacacffb2
Norbert Tóth <norbert@quince.hu>
2018-07-05 00:48:31 +0200

    Fix them linter errors ✨

--- calculateDueDate/_tests/isWorkingHours.test.js
+++ calculateDueDate/_tests/isWorkingHours.test.js
@@ -1,9 +1,9 @@
-import isWorkingHours from './isWorkingHours.js';
+import isWorkingHours from '../modules/isWorkingHours.js';
 import emarsys from '../config';
 
 const testDate1 = new Date('July 3 2018 13:30');
 const testDate2 = new Date('July 3 2018 8:59');
-const testDate3 = new Date('July 3 2018 17:01');
+// const testDate3 = new Date('July 3 2018 17:01');
 const testDate4 = new Date('July 3 2018 8:30');
 const testDate5 = new Date('July 3 2018 20:30');
 const testDate6 = new Date('July 1 2018 12:30');
--- index.mjs
+++ index.mjs
@@ -1,5 +1,7 @@
 import emarsys from './calculateDueDate/config.js';
 import CalculateDueDate from './calculateDueDate/index.js';
-const testDate5 = new Date('July 3 2018 10:30');
 
-console.log(CalculateDueDate(16, emarsys, testDate5));
+
+const submit = new Date('July 3 2018 10:30');
+
+console.log(CalculateDueDate(41, emarsys, submit));
