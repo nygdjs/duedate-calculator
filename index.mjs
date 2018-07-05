c1d00fe9645317fac272e4c787cef134a3cbb39f
Norbert Tóth <norbert@quince.hu>
2018-07-05 04:29:27 +0200

    Fix linter warning and test script

--- index.mjs
+++ index.mjs
@@ -1,3 +1,5 @@
+/* eslint no-console: 0 */
+
 import emarsys from './calculateDueDate/config.js';
 import getDayString from './calculateDueDate/modules/getDayString.js';
 import CalculateDueDate from './calculateDueDate/calculateDueDate.js';
@@ -6,7 +8,7 @@ import CalculateDueDate from './calculateDueDate/calculateDueDate.js';
 const submit = new Date('July 3 2018 10:30');
 
 console.log(`
-    submit: ${submit},
+    submitted: ${submit},
     working hours: ${emarsys.hours.start}h - ${emarsys.hours.end}h,
     working days: ${getDayString[emarsys.week.start]} - ${getDayString[emarsys.week.end]},
     turnaround: 32h
@@ -14,7 +16,7 @@ console.log(`
 `);
 
 // If you don't specify a submit date, it defaults to now
-console.log(`
-    If you don't specify a submit date, it defaults to now
-    ${CalculateDueDate(41, emarsys)}
-`);
+// console.log(`
+//     If you don't specify a submit date, it defaults to now
+//     ${CalculateDueDate(41, emarsys)}
+// `);
