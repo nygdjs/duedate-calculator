6766148b744453381c9589fc0b69fe5457c84b03
Norbert Tóth <norbert@quince.hu>
2018-07-05 03:34:16 +0200

    Add lint to test script

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import emarsys from './calculateDueDate/config.js';
-import CalculateDueDate from './calculateDueDate/index.js';
+import CalculateDueDate from './calculateDueDate/calculateDueDate.js';
 
 
 const submit = new Date('July 3 2018 10:30');
 
-console.log(CalculateDueDate(41, emarsys, submit));
+console.log(CalculateDueDate(9, emarsys, submit));
